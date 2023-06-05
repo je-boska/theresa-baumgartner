4bf2b88ed2509e4fc91b799d55fd6f7be5f7e712
je-boska <joneirikboska@gmail.com>
2023-06-05 14:34:40 +0200

    Add suspense boundary to /projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,16 @@
 import { getPosts } from '@/queries/Post';
 import Projects from '../../components/Projects';
 import Container from '@/components/Container';
+import { Suspense } from 'react';
 
 export default async function projects() {
   const posts = await getPosts();
 
   return (
     <Container>
-      <Projects posts={posts} />
+      <Suspense>
+        <Projects posts={posts} />
+      </Suspense>
     </Container>
   );
 }
