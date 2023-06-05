d0360be0f57e2733b2fc1a59f2e3263a774c36ae
je-boska <joneirikboska@gmail.com>
2023-06-05 14:29:55 +0200

    Save changes to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 
 import Container from '@/components/Container';
-import { SidebarContext } from './contexts/SidebarContext';
 
 export default async function page() {
   return (
-    <SidebarContext.Provider value={true}>
-      <Container>
-        <h1 className='mb-2 text-2xl font-medium uppercase'>
-          Theresa Baumgartner
-        </h1>
-        <p>Berlin based visual artist</p>
-      </Container>
-    </SidebarContext.Provider>
+    <Container>
+      <h1 className='mb-2 text-2xl font-medium uppercase'>
+        Theresa Baumgartner
+      </h1>
+      <p>Berlin based visual artist</p>
+    </Container>
   );
 }
