8b07bb1afe4f8fd4887aa906f20e73d2ea6422dc
je-boska <joneirikboska@gmail.com>
2023-06-07 17:08:25 +0200

    Apply responsive title styling to all headings

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,9 @@ export default function contact() {
   return (
     <Container className='m-4'>
       <Section>
-        <h1 className='mb-2 text-2xl font-medium uppercase'>Contact</h1>
+        <h1 className='mb-2 font-title text-2xl font-medium uppercase md:text-3xl lg:text-5xl'>
+          Contact
+        </h1>
         <p>Instagram</p>
         <p>Vimeo</p>
       </Section>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function page() {
   return (
     <Container className='m-4'>
       <Section>
-        <h1 className='mb-2 text-2xl font-medium uppercase'>
+        <h1 className='mb-2 font-title text-2xl font-medium uppercase md:text-3xl lg:text-5xl'>
           Theresa Baumgartner
         </h1>
         <p>Berlin based visual artist</p>
